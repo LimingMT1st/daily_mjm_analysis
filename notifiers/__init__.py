b2229e6f3cf38996e932ab6a01af5ead41fdbcce
LimingMT1st <49525776+LimingMT1st@users.noreply.github.com>
2026-06-08 08:08:55 +0800

    Add Feishu bot notifier support

--- notifiers/__init__.py
+++ notifiers/__init__.py
@@ -1,6 +1,13 @@
 from .manager import NotifierManager
 from .email_notifier import EmailNotifier
+from .feishu_notifier import FeishuNotifier
 from .telegram_notifier import TelegramNotifier
 from .wecom_notifier import WeComNotifier
 
-__all__ = ["NotifierManager", "EmailNotifier", "TelegramNotifier", "WeComNotifier"]
+__all__ = [
+    "NotifierManager",
+    "EmailNotifier",
+    "FeishuNotifier",
+    "TelegramNotifier",
+    "WeComNotifier",
+]
